d5f3eb3368ce26f36a9f92de343fd87a928f0313
Cameron Wolfe <cwolfe@cwolfe-ltm1.internal.salesforce.com>
2020-02-07 17:59:29 -0500

    tried to fix

--- neural_net.py
+++ neural_net.py
@@ -1,183 +1,127 @@
-"""This file contains my personal implementation of a simple neural network with
-forward and backward propogation that I used for several machine learning projects
-"""
+"""simple implementation of neural network"""
 
 import numpy as np
+import random
 
-from neural_net_help import create_weight_mat, activate, sig_deriv, visualize_cost
+from neural_net_help import (
+    create_weight_mat,
+    relu,
+    d_drelu,
+    sigmoid,
+    d_dsigmoid,
+    visualize_cost,
+)
 
-class Neural_Network:
-	"""Class for neural network object"""
+class FFNN:
+    """Class for neural network object"""
 
-	def __init__(self, layer_sizes):
-		"""Constructor for neural net object, takes in a list
-		of integers where each integer in the list represents
-		the number of hiddens nodes in a certain layer (including
-		input/output layers)
+    def __init__(self, layer_sizes):
+        """Constructor for neural net object, takes in a list
+        of integers where each integer in the list represents
+        the number of hiddens nodes in a certain layer (including
+        input/output layers)
 		
-		Parameters:
-		layer_sizes-- list of number of nodes present in each layer
-		"""
+        Parameters:
+        layer_sizes-- list of number of nodes present in each layer
+        """
 
-		self.layer_sizes = layer_sizes
-		self.layers = len(layer_sizes)
-		self.weights = []
+        self.layer_sizes = layer_sizes
+        self.layers = len(layer_sizes)
+        self.weights = []
 		
-		# use to store intermediate values when activating
-		self.values = [] 
-		self.act_values = []
+        # use to store intermediate values when activating
+        self.values = [] 
+        self.act_values = []
 
-		# create all initial weight matrices with proper sizes
-		for ind in range(1, len(layer_sizes)):
-			# matrix should have number columns equal to current input layer
-			# and number rows equal to number of resulting output nodes
-			rows = layer_sizes[ind]
-			cols = layer_sizes[ind - 1] + 1 # must add extra weights for bias
-			self.weights.append(create_weight_mat((rows, cols)))
+        # create kaiming initialized weight matrices
+        for ind in range(1, len(layer_sizes)):
+            num_in = layer_sizes[ind - 1] + 1 # account for bias
+            num_out = layer_sizes[ind]
+            self.weights.append(create_weight_mat((num_in, num_out)))
 
-	def forward_prop(self, inputs):
-		"""Forward propogates the neural network and returns the
-		resulting output value(s)
+        # store gradient in loss for backprop
+        self.loss_grad = np.zeros((1, 1))
 
-		Parameters:
-		inputs -- the inputs into the neural network - type is expected
-		to be a normal python list
-		"""
-		
-		# make sure input is the correct length
-		if(len(inputs) != self.layer_sizes[0]):
-			print("Error: {0} inputs were expected and {1} were given. \
-				Returning null...".format(str(self.layer_sizes[0]), 
-				str(len(inputs))))
-		
-			return None
+    def bce_loss(self, output, target):
+        """binary cross entropy loss function -- stores the loss
+        gradient for backpropogation
+
+        output: output array of size B X 1
+        target: target array of size B X 1
+        """
+
+        loss = -((1 - target)*np.log(1 - output)
+                + target*np.log(output))
+        self.loss_grad = (target - output)
+        return float(loss)
+        
+
+    def forward(self, inputs):
+        """Forward propogates the neural network and returns the
+        resulting output value(s)
+
+        Parameters:
+        inputs -- the inputs into the neural network - type is expected
+        to be a numpy array of size b x n
+        """
 	
-		# clear intermediate values so the list can be repopulated
-		self.values = []
-		self.act_values = []
+        # clear intermediate values so the list can be repopulated
+        # these values must be tracked for use in backward pass
+        self.z_vals = []
+        self.a_vals = []
 
-		#transform input into a numpy array that can be dot multiplied
-		inputs = np.array([inputs])
-		inputs = np.transpose(inputs)
-		self.values.append(inputs)
-		self.act_values.append(inputs)
+        # transform into numpy array
+        self.z_vals.append(inputs)
 
-		# forward activate the inputs through all of the weight matrices
-		curr_vals = inputs
-		for weight_mat in self.weights:
-			# must stack a bias unit on top
-			vals = np.dot(weight_mat, np.vstack([np.ones((1,1)), curr_vals]))
-			# store intermediate values to be used by backpropogation
-			# must store both unactivated and activated values for backpropogation
-			self.values.append(vals)
-			self.act_values.append(activate(vals))
-			curr_vals = activate(vals)
-		
-		return curr_vals
+        # forward activate the inputs through all of the weight matrices
+        curr_vals = inputs
+        for weight_mat in self.weights:
+            self.a_vals.append(curr_vals)
+            
+            # must stack a bias unit on top
+            bias = np.ones((curr_vals.shape[0], 1))
+            data_in = np.hstack((curr_vals, bias))
+            z_val = np.dot(data_in, weight_mat)
+            a_val = relu(z_val)
+            self.z_vals.append(z_val)
+            curr_vals = a_val
+        return sigmoid(curr_vals)
 
-	def backward_prop(self, output, expected, alpha=.01, grad_check=False, inputs=None):
-		"""Backward propogates the neural network to update the weights
-		based on a current training example given to the network
-
-		Parameters:
-		inputs -- the inputs for the current example (should be a np array)
-		outputs -- the actual outputs of the neural network
-		expected -- the expected outputs of the neural network
-		NOTE: expected and outputs should both be (N X 1)
-		
-		a list of the numpy arrays containing the gradient for each matrix is
-		returned, empty if grad_check is false
-		"""
-		
-		# must track delta as you backpropogate
-		delta = None
-		for layer in reversed(range(1, self.layers)):
-			# backprop for output layer
-			if(layer == self.layers - 1):
-				error = expected - output
-				delta = np.multiply(error, sig_deriv(output))
-			
-				# add changes based on deltas into the weight matrix
-				updates = np.vstack([np.ones((1,1)), self.act_values[layer - 1]]).dot(delta).T
-				self.weights[layer - 1] += np.multiply(alpha, updates)
-				
-				
-				# if gradient is being checked, add to the current list
-				if grad_check:
-					grad_arr = np.zeros(self.weights[layer - 1].shape)
-					for r in range(self.weights[layer - 1].shape[0]):
-						for c in range(self.weights[layer - 1].shape[1]):
-							grad_arr[r][c] = self.compute_numerical_gradient(inputs, expected, layer-1, r, c, .0001)		
-					print("\n\nBACKPROP DELTA:\n")
-					print(np.multiply(alpha, updates))
-					print("\n\nGRADIENT CHECK\n")
-					print(grad_arr)
-					input()
-				
-			
-			# backprop for all hidden layers
-			else:
-				# must eliminate bias from the error
-				error = delta.dot(self.weights[layer][:, 1:]) # 1 X 2
-				delta = np.multiply(error, sig_deriv(self.act_values[layer]).T) # 2 X 1 - multiply upper gradient by gradient at current node
-				
-				# find updates to weights and add values to weight matrix	
-				updates = np.vstack([np.ones((1,1)), self.act_values[layer - 1]]).dot(delta).T
-				self.weights[layer - 1] += np.multiply(alpha, updates)			
-				"""	
-				# determine the gradient check matrix and check to see if they are the same
-				if grad_check:
-					grad_arr = np.zeros(self.weights[layer - 1].shape)
-					for r in range(self.weights[layer - 1].shape[0]):
-						for c in range(self.weights[layer - 1].shape[1]):
-							grad_arr[r][c] = self.compute_numerical_gradient(inputs, expected, layer-1, r, c, .0001)
-					print("\n\nBACKPROP DELTA:\n")
-					print(updates)
-					print("\n\nGRADIENT CHECK\n")
-					print(grad_arr)
-					input()
-				"""
-
-	def compute_numerical_gradient(self, ins, out, layer, r, c, epsilon):
-		"""this method computes the numerical gradient of the neural network that
-		can be compared to the actual gradient to ensure that backpropogation is
-		working properly"""
-		
-		# perturb the weight upwards and compute the output
-		self.weights[layer][r, c] += epsilon
-		JUp = out - self.forward_prop(ins)
-		
-		# perturb the weight downwards and compute the output
-		self.weights[layer][r, c] -= (2*epsilon)
-		JLow = out - self.forward_prop(ins)
-
-		# calculate numerical gradient and return
-		self.weights[layer][r, c] += epsilon
-		grad = (JUp - JLow)/(2*epsilon)
-		return grad
+    def backward(self, outputs, alpha:float=.1):
+        error = self.loss_grad*d_dsigmoid(outputs)
+        for ind in reversed(range(1, self.layers)):
+            weights = self.weights[ind - 1]
+            prev_acts = self.a_vals[ind - 1]
+            input_vec = np.vstack((np.transpose(prev_acts), np.ones((1, 1))))
+            weight_grad = np.dot(input_vec, error)
+            self.weights[ind - 1] += alpha*weight_grad # update weights
+            if ind > 1:
+                prev_z = self.z_vals[ind - 1]
+                act_grad = np.dot(error, np.transpose(weights))
+                act_grad = act_grad[:, :-1] # remove bias
+                error = act_grad*d_drelu(prev_z)
 
 if  __name__ == '__main__':
-	"""Used for simple testing"""
-	
-	nn = Neural_Network([2,3, 3, 1])
-	inputs = [[1,1],[0,1],[1,0],[0,0]]
-	expected = [0, 1, 1, 0]
-	expected_np = np.array(expected, copy=True)
-	loss_func_vals = []
-	for x in range(100000):
-		results = []
-		cost = 0.0
-		for ins, exp in zip(inputs, expected):
-			output = nn.forward_prop(ins)
-			results.append(output)
-			nn.backward_prop(output, np.array(exp).T, alpha=.001, grad_check=False, inputs=ins)
-			cost += np.square(output - exp)[0, 0]
-		loss_func_vals.append(cost)	
-		
-	visualize_cost(loss_func_vals)	
-	
-	print(nn.forward_prop([0,0]))
-	print(nn.forward_prop([1,0]))
-	print(nn.forward_prop([0,1]))
-	print(nn.forward_prop([1,1]))
-		
+    """Used for simple testing"""
+
+    # train the neural net on xor	
+    nn = FFNN([2, 8, 1])
+    inputs = np.array([[1,1],[0,1],[1,0],[0,0]])
+    expected = np.transpose(np.array([[0, 1, 1, 0]]))
+    losses = []
+    for x in range(1000):
+        tmp_losses = 0.
+        for ind in range(inputs.shape[0]):
+            data_in = inputs[ind, :][None, :]
+            target = expected[ind, :][None, :]
+            output = nn.forward(data_in)
+            tmp_loss = nn.bce_loss(output, target)
+            nn.backward(output)
+            tmp_losses += tmp_loss
+        tmp_losses /= 4
+        if x % 100 == 0:
+            print(f'Loss: {tmp_losses}')
+    print(f'0 --> {nn.forward(inputs[0][None, :])}')
+    print(f'1 --> {nn.forward(inputs[1][None, :])}')
+    print(f'1 --> {nn.forward(inputs[2][None, :])}')
+    print(f'0 --> {nn.forward(inputs[3][None, :])}')
--- neural_net_help.py
+++ neural_net_help.py
@@ -1,6 +1,4 @@
-"""This file contains all helper functions that were used in the implementation of
-the neural network
-"""
+"""helper methods for implementing neural network"""
 
 # must include this to avoid backend error message with mac
 import matplotlib
@@ -8,50 +6,47 @@ matplotlib.use("TkAgg")
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.special import expit
 
 def create_weight_mat(size):
-	"""Creates a matrix with randomly initialized values between
-	-.5 and .5"""
+    """creates a weight matrix with kaiming initialization"""
 
-	weight_mat = np.random.rand(size[0], size[1])
-	weight_mat -= .5
+    std = np.sqrt(2/size[0]) # fan in
+    weights = np.random.normal(0, 1.0, size=size)
+    return weights
 
-	return weight_mat
+def relu(values):
+    """relu activation function"""	
+    values[values < 0] = 0.
+    return values
 
-def activate(values):
-	"""applies a sigmoid activtion function to a given list of values
-	and returns the values"""
-	
-	return expit(values)
+def d_drelu(grad):
+    grad[grad < 0] = 0.
+    grad[grad >= 0] = 1.
+    return grad
 
-def sig_deriv(values):
-	"""Helper method to pass values through the derivative of the sigmoid
-	function - used for backpropogation of neural network
-	
-	*Assumed that values has already been passed through sigmoid activation
-	"""
-	
-	return values*(1 - values)
+def sigmoid(values):
+    return 1./(1. + np.exp(-values))
+
+def d_dsigmoid(grad):
+    """assumed that the input is activation values not pre-activation"""
+    return grad*(1. - grad)
 
 def visualize_cost(error_vals):
-	"""Method for visualizing the cost function that the neural network
-	is optimizing to ensure that gradient descent is working properly
-	"""
+    """Method for visualizing the cost function that the neural network
+    is optimizing to ensure that gradient descent is working properly
+    """
 	
-	plt.title("Cost Function Visualization")
-	plt.ylabel("Cost")
-	plt.xlabel("Iteration")
-	plt.plot(error_vals)
-	plt.show()
+    plt.title("Cost Function Visualization")
+    plt.ylabel("Cost")
+    plt.xlabel("Iteration")
+    plt.plot(error_vals)
+    plt.show()
 
 if __name__ == '__main__':
-	"""Use for simple tests"""
+    """Use for simple tests"""
 
-	mat = create_weight_mat((1,6))
-	print(mat)
+    mat = create_weight_mat((1,6))
+    print(mat)
 	
-	mat_1 = create_weight_mat((3,4))
-	print(mat_1)
-
-
+    mat_1 = create_weight_mat((3,4))
+    print(mat_1)
